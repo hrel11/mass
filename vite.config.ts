2bff734380df16b8e44ff663ae714ad5d96e8dc4
mk <hrel11@yahoo.co.jp>
2025-04-12 10:45:31 +0900

    feat: create rrv7 app

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  // npm run devでホストからアクセスできるようにする
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+    allowedHosts: ['dev-meraku.com'],
+  },
+});
