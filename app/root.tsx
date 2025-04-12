1bef27e358fe1f87f32c9819cf3924cb2e23ea48
mk <hrel11@yahoo.co.jp>
2025-04-13 00:52:23 +0900

    feat: sample app

--- app/root.tsx
+++ app/root.tsx
@@ -1,75 +1,139 @@
+import type { LinksFunction, LoaderFunctionArgs } from 'react-router';
+
+import { useEffect, useState } from 'react';
+
+import appStylesHref from './app.css?url';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: appStylesHref },
+];
+import { redirect } from 'react-router';
+
 import {
-  isRouteErrorResponse,
+  Form,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "react-router";
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from 'react-router';
 
-import type { Route } from "./+types/root";
-import "./app.css";
+import { createEmptyContact, getContacts } from './data';
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return { contacts, q };
+};
+
+export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  // クエリは状態として保持する必要があります
+  const [query, setQuery] = useState(q || '');
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
+
+  // 戻る/進むボタンをクリックしたときにクエリをコンポーネントの状態に同期させる`useEffect`は残っています
+  useEffect(() => {
+    setQuery(q || '');
+  }, [q]);
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body suppressHydrationWarning={true}>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form
+              id="search-form"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              role="search"
+            >
+              <input
+                aria-label="Search contacts"
+                className={searching ? 'loading' : ''}
+                id="q"
+                name="q"
+                // ユーザーの入力をコンポーネントの状態に同期
+                onChange={(event) => setQuery(event.currentTarget.value)}
+                placeholder="Search"
+                type="search"
+                // `defaultValue`から`value`に変更
+                value={query}
+              />
+
+              <div aria-hidden hidden={!searching} id="search-spinner" />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? 'active' : isPending ? 'pending' : ''
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>名前なし</i>
+                      )}{' '}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>連絡先なし</i>
+              </p>
+            )}
+          </nav>
+        </div>
+        <div
+          className={
+            navigation.state === 'loading' && !searching ? 'loading' : ''
+          }
+          id="detail"
+        >
+          <Outlet />
+        </div>
+
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  );
-}
-
-export default function App() {
-  return <Outlet />;
-}
-
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
   );
 }
---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      これはRRv7のデモです
+      <br />
+      <a href="https://remix.run">remix.runのドキュメント</a>をご覧ください。
+    </p>
+  );
+}
