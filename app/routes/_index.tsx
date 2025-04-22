33ace7abfeddb93848e9bacf83dc3a1d97d1300d
mk <hrel11@yahoo.co.jp>
2025-04-22 13:53:44 +0900

    fix: add logo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,22 @@
+import remixLogo from '~/remixLogo.svg';
+
 export default function Index() {
   return (
     <>
-      <p
-        id="index-page"
-        className="mx-2xl my-auto text-center text-[#818181] before:mb-[0.5rem] before:block"
-      >
-        これはRRv7のデモです
-        <br />
-        <a
-          href="https://remix.run"
-          className="text-inherit hover:text-[#121212]"
-        >
-          remix.runのドキュメント
-        </a>
-        をご覧ください。
-      </p>
+      <article className="flex flex-col items-center gap-[0.5rem] text-center text-[#818181]">
+        <img src={remixLogo} alt="logo" />
+        <p>
+          これはRRv7のデモです
+          <br />
+          <a
+            href="https://remix.run"
+            className="text-inherit hover:text-[#121212]"
+          >
+            remix.runのドキュメント
+          </a>
+          をご覧ください。
+        </p>
+      </article>
     </>
   );
 }
